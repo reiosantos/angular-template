0cd6caeeaacbadce2176014aa1298c38b0e9c9ba
santos <ronireiosantos@gmail.com>
2020-09-03 19:30:28 +0300

    - Fix prefix URL

--- docker/run.sh
+++ docker/run.sh
@@ -8,6 +8,7 @@ if [[ ${ENV} == 'local' ]]; then
 else
     touch /var/run/supervisor.sock
     mkdir -p /usr/share/nginx/html/dashboard/
+    rm -rf /usr/share/nginx/html/dashboard/*
     cp -R ${APP_PATH}/dist/angular-template/* /usr/share/nginx/html/dashboard/
     cp ${APP_PATH}/nginx.conf /etc/nginx/conf.d/default.conf
     cp ${APP_PATH}/supervisor.conf /etc/supervisor/conf.d/
